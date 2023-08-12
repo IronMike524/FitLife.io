a12a0e07e8355b43a32df22e1b33659d1c6f23d6
Jeixxon17 <jeison.martinez-l@uniminuto.edu.co>
2023-08-12 11:30:05 -0500

    Utilizando PHP y Conexion BD

---
+++ pages/Login/login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>FitLife</title>
+ <!-- Design by foolishdeveloper.com -->
+ <!-- Modificado por: Miguel Vélez -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+
+        <div class="container">
+            <div class="box-back">
+                <div class="box-back-login">
+                    <h3>¿Ya tiene una cuenta?</h3>
+                    <p>Inicia Sesion Para Conocer Mas Informacion</p>
+                    <button id="btn-login">Iniciar Sesion</button>
+                </div>
+                <div class="box-back-register">
+                    <h3>¿Aun no tienes una cuenta?</h3>
+                    <p>Registrate para que puedas iniciar sesion</p>
+                    <button id="btn-register">Registrase</button>
+                </div>
+            </div>
+            
+            <div class="container-login-register">
+                <form action="" class="form-login">
+                    <h2>Iniciar Sesion</h2>
+                    <input type="text" placeholder="Correo Electronico">
+                    <input type="password" placeholder="Contraseña">
+                    <button>Iniciar Sesion</button>
+                </form>
+
+                <form action="" class="form-register">
+                    <h2>Registrarse</h2>
+                    <input type="text" placeholder="Ingrese Su Nombre">
+                    <input type="text" placeholder="Ingrese Su Correo">
+                    <input type="password" placeholder="Ingrese Su Contraseña">
+                    <input type="password" placeholder="Ingrese Nuevamente Su Contraseña">
+                    <button>Registrase</button>
+                </form>
+            </div>
+        </div>
+
+    </main>
+    <script src="script.js"></script>
+</body>
+</html>
+
---
+++ pages/Login/script.js
@@ -0,0 +1,25 @@
+document.getElementById("btn-register").addEventListener("click", register);
+document.getElementById("btn-login").addEventListener("click", login);
+
+
+var container_login_register = document.querySelector(".container-login-register")
+var form_login = document.querySelector(".form-login");
+var form_register = document.querySelector(".form-register");
+var box_back_login = document.querySelector(".box-back-login");
+var box_back_register = document.querySelector(".box-back-register");
+
+function login() {
+    form_register.style.display = "none";
+    container_login_register.style.left = "10px";
+    form_login.style.display = "block";
+    box_back_register.style.opacity = "1";
+    box_back_login.style.opacity = "0"
+}
+
+function register() {
+    form_register.style.display = "block";
+    container_login_register.style.left = "410px";
+    form_login.style.display = "none";
+    box_back_register.style.opacity = "0";
+    box_back_login.style.opacity = "1"
+}
