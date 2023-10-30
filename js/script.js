be71ebff33ae4630785b8830d1d108ccbde011ab
Jeixxon17 <jeison.martinez-l@uniminuto.edu.co>
2023-10-30 16:02:16 -0500

    Lista Ejercicios

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ let nav = document.querySelector('.nav');
 document.querySelector('#menu-btn').onclick = () =>{
     nav.classList.toggle('active');
 }
+
+/*function MostrarEjercicios() {
+    document.getElementById('exercises').style.display = 'block';
+} */
+
+
+
+  
